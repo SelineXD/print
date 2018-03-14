824f24a6f3f82c15c5ae04cacfd32279e1a86be0
SelineXD <seline.and2@gmail.com>
2018-03-14 16:02:31 +0100

    SEL
    Manipulation bei den Buttons

--- eingeloggt.php
+++ eingeloggt.php
@@ -11,16 +11,15 @@ include("connection.php");
 ?>
 	<div class="container">
 		<div id="logo">Printers</div>
+			<a href="contact.php">Kontakt</a>
+			<a href="angebot2.php">Angebot </a>
+			<a href="logout.php">Logout</a>
 		<div id="share-buttons">
 			<a href="http://www.facebook.com/sharer.php?u=https://simplesharebuttons.com" target="_blank">
 	        		<img src="https://simplesharebuttons.com/images/somacro/facebook.png" alt="Facebook" /></a>
 			<a href="https://twitter.com/share?url=https://simplesharebuttons.com&amp;text=Simple%20Share%20Buttons&amp;hashtags=simplesharebuttons" target="_blank">
 	        		<img src="https://simplesharebuttons.com/images/somacro/twitter.png" alt="Twitter" /></a>
-			</div>
-			<a href="contact.php">Kontakt</a>
-			<a href="angebot2.php">Angebot </a>
-			<a href="logout.php">Logout</a>
-			
+			</div>	
 		</div> <!--Ende container-->
 	</div>
 	<div id="content">
--- index.php
+++ index.php
@@ -9,16 +9,15 @@
 	<div id="header">
 	<div class="container">
 		<div id="logo">Printers</div>
+			<a href="index.php">Home</a>
+			<a href="angebot.php">Angebot </a>
+			<a href="login.php">Login </a>
 		<div id="share-buttons">
 			<a href="http://www.facebook.com/sharer.php?u=https://simplesharebuttons.com" target="_blank">
 	        		<img src="https://simplesharebuttons.com/images/somacro/facebook.png" alt="Facebook" /></a>
 			<a href="https://twitter.com/share?url=https://simplesharebuttons.com&amp;text=Simple%20Share%20Buttons&amp;hashtags=simplesharebuttons" target="_blank">
 	        		<img src="https://simplesharebuttons.com/images/somacro/twitter.png" alt="Twitter" /></a>
-			</div>
-			<a href="index.php">Home</a>
-			<a href="angebot.php">Angebot </a>
-			<a href="login.php">Login </a>
-			
+		</div>	
 		</div> <!--Ende container-->
 	</div>
 	<div id="content">
