6eeb278a31785c060ce30d526e85f4815291c900
SelineXD <seline.and2@gmail.com>
2018-04-11 16:29:40 +0200

    Seline

--- users.sql
+++ users.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.4deb1
+-- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: May 31, 2017 at 02:34 PM
--- Server version: 5.6.30-1
--- PHP Version: 7.0.11-1
+-- Host: 127.0.0.1
+-- Erstellungszeit: 28. Mrz 2018 um 15:47
+-- Server-Version: 10.1.26-MariaDB
+-- PHP-Version: 7.1.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -17,53 +19,29 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `soccerplayer`
+-- Datenbank: `print`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Tabellenstruktur für Tabelle `users`
 --
 
 CREATE TABLE `users` (
   `id` int(10) UNSIGNED NOT NULL,
-  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `passwort` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `vorname` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `nachname` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `passwort` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `vorname` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `nachname` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `adresse` text NOT NULL,
+  `ort` text NOT NULL,
+  `plz` int(5) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+COMMIT;
 
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `email`, `passwort`, `vorname`, `nachname`, `created_at`, `updated_at`) VALUES
-(1, 'seline.and@hotmail.ch', '$2y$10$0V/QNr1JEr2niR4TXh0DpuJmA/bCQWm5wIn0Q5MrvO3QGN2xszIOq', '', '', '2017-02-22 13:41:58', NULL),
-(2, 'seline.and@hotmail.com', '$2y$10$JXZz30f0g.Og.M.QznWKp.vV8vlyosWL66Y2UUt0yhnzgti5.uEey', '', '', '2017-05-31 11:59:41', NULL);
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
