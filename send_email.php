8a985d206a17ffb308e34dd204f988a593e171b1
SelineXD <seline.and2@gmail.com>
2018-04-18 16:42:08 +0200

    Seline
    löschung

--- send_email.php
+++ send_email.php
@@ -110,8 +110,7 @@ $mail_senden = mail($empfaenger,$betreff,$msg,$header);
 if($mail_senden){
   header("Location: ".$url_ok); //Mail wurde gesendet
   exit();
-} else{
-	echo $
+} else{	
   header("Location: ".$url_fehler); //Fehler beim Senden
   exit();
 }
